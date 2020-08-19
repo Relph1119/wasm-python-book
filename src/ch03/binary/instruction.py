90b739855caf67d9cf4fa06ad6d3cfdf44d8fdd3
Relph1119 <huruifeng1202@163.com>
2020-08-19 20:52:07 +0800

    添加ch03的代码，删除ch02/test目录

---
+++ src/ch03/binary/instruction.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: HuRuiFeng
+@file: instruction.py
+@time: 2020/8/18 18:28
+@project: wasm-python-book
+@desc:
+"""
+from ch03.binary.opnames import opnames
+
+
+class Expr(list):
+    """
+    表达式
+    expr  : instr*|0x0b
+    """
+
+    def __init__(self):
+        super().__init__()
+
+
+class Instruction:
+    """指令"""
+
+    def __init__(self):
+        # 操作码
+        self.opcode = None
+        # 操作数
+        self.args = None
+
+    def get_opname(self):
+        return opnames[self.opcode]
+
+    def __str__(self):
+        return opnames[self.opcode]
+
+
+class BlockArgs:
+    """block和loop指令的参数"""
+
+    def __init__(self):
+        # block type:
+        # -1表示i32类型结果，-2表示i64类型结果，
+        # -3表示f32类型结果，-4表示f64类型结果，
+        # -64表示没有结果
+        self.bt = None
+        # 内嵌的指令序列
+        self.instrs = []
+
+
+class IfArgs:
+    """if指令的参数"""
+
+    def __init__(self):
+        # block type
+        self.bt = None
+        self.instrs1 = []
+        self.instrs2 = []
+
+
+class BrTableArgs:
+    """br_table指令的参数"""
+
+    def __init__(self, labels=None, default=None):
+        # 跳转表
+        if labels is None:
+            labels = []
+        self.labels = labels
+        # 默认跳转标签
+        self.default = default
+
+
+class MemArg:
+    """内存指令参数"""
+
+    def __init__(self, align=None, offset=None):
+        # 对齐提示
+        self.align = align
+        # 内存偏移量
+        self.offset = offset
---
+++ src/ch03/cmd/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: HuRuiFeng
+@file: Main.py
+@time: 2020/8/18 16:23
+@desc: 主函数
+"""
+import os
+from optparse import OptionParser
+
+from ch03 import binary
+from ch03.cmd.dumper import dump
+
+
+def main(input_args):
+    # 设置传入参数
+    parser = OptionParser(usage="usage:%prog [-d] filename")
+
+    parser.add_option("-d", "--dump", action="store_true", default=False, dest="dump_flag",
+                      help="print version and exit.")
+    # 解析参数
+    (options, args) = parser.parse_args(input_args)
+    module, err = binary.decode_file(args[0])
+
+    if options.dump_flag:
+        dump(module)
+
+
+if __name__ == '__main__':
+    # 打印帮助
+    # fake_args = ['-h']
+    # main(fake_args)
+
+    # 使用输入参数测试
+    root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    file_name = os.path.join(os.path.dirname(root_path), "../wat", "ch03_eg4_block.wasm")
+    fake_args = ['-d', file_name]
+    main(fake_args)
